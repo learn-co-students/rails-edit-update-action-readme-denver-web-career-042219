baebc0952528df2e3ad0216153fe5ce082aa870e
brookton <davidbrookton@gmail.com>
2019-05-17 20:05:49 -0600

    Done.

--- db/migrate/20190518012750_article.rb
+++ db/migrate/20190518012750_article.rb
@@ -1,10 +1,8 @@
-class CreateArticles < ActiveRecord::Migration
+class Article < ActiveRecord::Migration[5.0]
   def change
     create_table :articles do |t|
       t.string :title
       t.text :description
-
-      t.timestamps null: false
     end
   end
 end
